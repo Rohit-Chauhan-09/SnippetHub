0ee6a311976cc71c43320df85959aebb51262068
Rohit-Chauhan-09 <rohitchauhan9112004@gmail.com>
2026-03-04 00:57:22 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000
 // Configure CORS for Frontend
 app.use(cors(
     {
-        origin: 'http://localhost:5173'
+       // origin: 'http://localhost:5173'
     }
 ))
 
